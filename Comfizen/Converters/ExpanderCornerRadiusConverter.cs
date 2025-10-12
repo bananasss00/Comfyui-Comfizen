2e20b4324a45d83cd3ddbd9e26c95c8450b59535
bananasss00 <bananasss00@yandex.ru>
2025-10-12 20:22:02 +0300

    fixes

---
+++ Comfizen/Converters/ExpanderCornerRadiusConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Comfizen;
+
+public class ExpanderCornerRadiusConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool isExpanded && isExpanded)
+        {
+            return new CornerRadius(3, 3, 0, 0);
+        }
+        return new CornerRadius(3);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
