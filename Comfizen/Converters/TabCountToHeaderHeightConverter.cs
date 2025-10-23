a2bb5ba50306ac3bea5f53c1169e87cd44d97a0f
bananasss00 <bananasss00@yandex.ru>
2025-10-23 22:23:37 +0300

    show tabcontrol when tabs > 1

---
+++ Comfizen/Converters/TabCountToHeaderHeightConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Comfizen;
+
+public class TabCountToHeaderHeightConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is int count && count > 1)
+        {
+            return new GridLength(1, GridUnitType.Auto);
+        }
+        return new GridLength(0);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Comfizen/Converters/TabCountToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Comfizen;
+
+public class TabCountToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is int count)
+        {
+            return count > 1 ? Visibility.Visible : Visibility.Collapsed;
+        }
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
